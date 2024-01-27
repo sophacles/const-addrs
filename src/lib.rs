f160708cd3fc49479e5952fc73311f7cc17af6df
Erich Heine <sophacles@gmail.com>
2024-01-27 15:25:20 -0600

    feat: tighten up the code with more macros!
    refactors the ip!, net!, and sock! macro innards into a relevant $ty_tokens,
    so that the parse_types macro can expand for them.
    
    Creates a macro_rules macro called: parse_types macro that removes
    the boilerplate inside the macro definitions.
    
    I want to make a enum_tok macro that goes futher, but i fear I may have
    gone too far already.

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,20 @@ impl Parse for ToAddr {
     }
 }
 
+macro_rules! parse_type {
+    ($input:ident, $fn:ident, $ty:ty) => {{
+        let ToAddr { inval } = parse_macro_input!($input as ToAddr);
+
+        match <$ty>::from_str(inval.value().as_str()) {
+            Ok(v) => $fn(Some(v)).into(),
+            Err(e) => {
+                emit_error!(inval, e);
+                $fn(None).into()
+            }
+        }
+    }};
+}
+
 fn ip4_tokens(addr: Option<Ipv4Addr>) -> proc_macro2::TokenStream {
     let addr = addr.unwrap_or(Ipv4Addr::UNSPECIFIED);
     let [a, b, c, d] = addr.octets();
@@ -32,56 +46,36 @@ fn ip6_tokens(addr: Option<Ipv6Addr>) -> proc_macro2::TokenStream {
     quote! { ::std::net::Ipv6Addr::new(#a, #b, #c, #d, #e, #f, #g, #h) }
 }
 
+fn ipaddr_tokens(addr: Option<IpAddr>) -> proc_macro2::TokenStream {
+    let addr = addr.unwrap_or(IpAddr::V4(Ipv4Addr::UNSPECIFIED));
+    match addr {
+        IpAddr::V4(ip) => {
+            let inner = ip4_tokens(Some(ip));
+            quote! { ::std::net::IpAddr::V4(#inner) }
+        }
+        IpAddr::V6(ip) => {
+            let inner = ip6_tokens(Some(ip));
+            quote! { ::std::net::IpAddr::V6(#inner) }
+        }
+    }
+}
+
 #[proc_macro]
 #[proc_macro_error]
 pub fn ip(input: TokenStream) -> TokenStream {
-    let ToAddr { inval } = parse_macro_input!(input as ToAddr);
-
-    match IpAddr::from_str(inval.value().as_str()) {
-        Ok(addr) => match addr {
-            IpAddr::V4(ip) => {
-                let inner = ip4_tokens(Some(ip));
-                quote! { ::std::net::IpAddr::V4(#inner) }.into()
-            }
-            IpAddr::V6(ip) => {
-                let inner = ip6_tokens(Some(ip));
-                quote! { ::std::net::IpAddr::V6(#inner) }.into()
-            }
-        },
-        Err(e) => {
-            emit_error!(inval, e);
-            let ip_tok = ip4_tokens(None);
-            quote! { ::std::net::IpAddr::V4(#ip_tok, 0) }.into()
-        }
-    }
+    parse_type!(input, ipaddr_tokens, IpAddr)
 }
 
 #[proc_macro]
 #[proc_macro_error]
 pub fn ip4(input: TokenStream) -> TokenStream {
-    let ToAddr { inval } = parse_macro_input!(input as ToAddr);
-
-    match Ipv4Addr::from_str(inval.value().as_str()) {
-        Ok(ip) => ip4_tokens(Some(ip)).into(),
-        Err(e) => {
-            emit_error!(inval, e.to_string());
-            ip4_tokens(None).into()
-        }
-    }
+    parse_type!(input, ip4_tokens, Ipv4Addr)
 }
 
 #[proc_macro]
 #[proc_macro_error]
 pub fn ip6(input: TokenStream) -> TokenStream {
-    let ToAddr { inval } = parse_macro_input!(input as ToAddr);
-
-    match Ipv6Addr::from_str(inval.value().as_str()) {
-        Ok(ip) => ip6_tokens(Some(ip)).into(),
-        Err(e) => {
-            emit_error!(inval, e);
-            ip6_tokens(None).into()
-        }
-    }
+    parse_type!(input, ip6_tokens, Ipv6Addr)
 }
 
 fn sock4_tokens(addr: Option<SocketAddrV4>) -> proc_macro2::TokenStream {
@@ -98,56 +92,36 @@ fn sock6_tokens(addr: Option<SocketAddrV6>) -> proc_macro2::TokenStream {
     quote! { ::std::net::SocketAddrV6::new(#ip, #port, 0, 0) }
 }
 
+fn sockaddr_tokens(addr: Option<SocketAddr>) -> proc_macro2::TokenStream {
+    let addr = addr.unwrap_or(SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, 0)));
+    match addr {
+        SocketAddr::V4(sock) => {
+            let sock_tok = sock4_tokens(Some(sock));
+            quote! { ::std::net::SocketAddr::V4(#sock_tok) }
+        }
+        SocketAddr::V6(sock) => {
+            let sock_tok = sock6_tokens(Some(sock));
+            quote! { ::std::net::SocketAddr::V6(#sock_tok) }
+        }
+    }
+}
+
 #[proc_macro]
 #[proc_macro_error]
 pub fn sock(input: TokenStream) -> TokenStream {
-    let ToAddr { inval } = parse_macro_input!(input as ToAddr);
-
-    match SocketAddr::from_str(inval.value().as_str()) {
-        Ok(sock) => match sock {
-            SocketAddr::V4(sock) => {
-                let sock_tok = sock4_tokens(Some(sock));
-                quote! { ::std::net::SocketAddr::V4(#sock_tok) }.into()
-            }
-            SocketAddr::V6(sock) => {
-                let sock_tok = sock6_tokens(Some(sock));
-                quote! { ::std::net::SocketAddr::V6(#sock_tok) }.into()
-            }
-        },
-        Err(e) => {
-            emit_error!(inval, e);
-            let sock_tok = sock4_tokens(None);
-            quote! { ::std::net::SocketAddr::V4(#sock_tok) }.into()
-        }
-    }
+    parse_type!(input, sockaddr_tokens, SocketAddr)
 }
 
 #[proc_macro]
 #[proc_macro_error]
 pub fn sock4(input: TokenStream) -> TokenStream {
-    let ToAddr { inval } = parse_macro_input!(input as ToAddr);
-
-    match SocketAddrV4::from_str(inval.value().as_str()) {
-        Ok(sock) => sock4_tokens(Some(sock)).into(),
-        Err(e) => {
-            emit_error!(inval, e);
-            sock4_tokens(None).into()
-        }
-    }
+    parse_type!(input, sock4_tokens, SocketAddrV4)
 }
 
 #[proc_macro]
 #[proc_macro_error]
 pub fn sock6(input: TokenStream) -> TokenStream {
-    let ToAddr { inval } = parse_macro_input!(input as ToAddr);
-
-    match SocketAddrV6::from_str(inval.value().as_str()) {
-        Ok(sock) => sock6_tokens(Some(sock)).into(),
-        Err(e) => {
-            emit_error!(inval, e);
-            sock6_tokens(None).into()
-        }
-    }
+    parse_type!(input, sock6_tokens, SocketAddrV6)
 }
 
 #[cfg(feature = "ipnet")]
@@ -170,56 +144,39 @@ fn net6_tokens(net: Option<Ipv6Network>) -> proc_macro2::TokenStream {
 }
 
 #[cfg(feature = "ipnet")]
+fn net_tokens(net: Option<IpNetwork>) -> proc_macro2::TokenStream {
+    let net = net.unwrap_or(IpNetwork::V4(
+        Ipv4Network::new(Ipv4Addr::UNSPECIFIED, 0).unwrap(),
+    ));
+    match net {
+        IpNetwork::V4(net) => {
+            let net_tok = net4_tokens(Some(net));
+            quote! { ipnetwork::IpNetwork::V4(#net_tok) }
+        }
+        IpNetwork::V6(net) => {
+            let net_tok = net6_tokens(Some(net));
+            quote! { ipnetwork::IpNetwork::V6(#net_tok) }
+        }
+    }
+}
+
+#[cfg(feature = "ipnet")]
 #[proc_macro]
 #[proc_macro_error]
 pub fn net(input: TokenStream) -> TokenStream {
-    let ToAddr { inval } = parse_macro_input!(input as ToAddr);
-
-    match IpNetwork::from_str(inval.value().as_str()) {
-        Ok(sock) => match sock {
-            IpNetwork::V4(net) => {
-                let net_tok = net4_tokens(Some(net));
-                quote! { ipnetwork::IpNetwork::V4(#net_tok) }.into()
-            }
-            IpNetwork::V6(net) => {
-                let net_tok = net6_tokens(Some(net));
-                quote! { ipnetwork::IpNetwork::V6(#net_tok) }.into()
-            }
-        },
-        Err(e) => {
-            emit_error!(inval, e);
-            let net_tok = net4_tokens(None);
-            quote! { ipnetwork::IpNetwork::V4(#net_tok) }.into()
-        }
-    }
+    parse_type!(input, net_tokens, IpNetwork)
 }
 
 #[cfg(feature = "ipnet")]
 #[proc_macro]
 #[proc_macro_error]
 pub fn net4(input: TokenStream) -> TokenStream {
-    let ToAddr { inval } = parse_macro_input!(input as ToAddr);
-
-    match Ipv4Network::from_str(inval.value().as_str()) {
-        Ok(net) => net4_tokens(Some(net)).into(),
-        Err(e) => {
-            emit_error!(inval, e);
-            net4_tokens(None).into()
-        }
-    }
+    parse_type!(input, net4_tokens, Ipv4Network)
 }
 
 #[cfg(feature = "ipnet")]
 #[proc_macro]
 #[proc_macro_error]
 pub fn net6(input: TokenStream) -> TokenStream {
-    let ToAddr { inval } = parse_macro_input!(input as ToAddr);
-
-    match Ipv6Network::from_str(inval.value().as_str()) {
-        Ok(net) => net6_tokens(Some(net)).into(),
-        Err(e) => {
-            emit_error!(inval, e);
-            net6_tokens(None).into()
-        }
-    }
+    parse_type!(input, net6_tokens, Ipv6Network)
 }
