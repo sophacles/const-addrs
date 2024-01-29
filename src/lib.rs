a83d630580d7e51821a889dd7dc653627f08eb76
Erich Heine <sophacles@gmail.com>
2024-01-28 19:47:18 -0600

    refactor: use syn's parse function rather than  the Parser machinery
    Also:
    * use quote repetition
    * some little cleanups
    * test for the parse function

--- src/lib.rs
+++ src/lib.rs
@@ -1,49 +1,53 @@
 use proc_macro::TokenStream;
+
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr, SocketAddrV4, SocketAddrV6};
 use std::str::FromStr;
 
-use proc_macro_error::{emit_error, proc_macro_error};
+use proc_macro_error::{abort_call_site, emit_error, proc_macro_error};
 use quote::quote;
-use syn::parse::{Parse, ParseStream, Result};
-use syn::{parse_macro_input, LitStr};
-
-struct ToAddr {
-    inval: LitStr,
-}
-
-impl Parse for ToAddr {
-    fn parse(input: ParseStream) -> Result<Self> {
-        let inval = input.parse::<LitStr>()?;
-        Ok(ToAddr { inval })
-    }
-}
+use syn::{parse, LitStr};
 
 macro_rules! parse_type {
     ($input:ident, $fn:ident, $ty:ty) => {{
-        let ToAddr { inval } = parse_macro_input!($input as ToAddr);
+        let inval = match parse::<LitStr>($input) {
+            Ok(ls) => ls,
+            Err(e) => {
+                abort_call_site!(
+                    e.to_string();
+                    help = "Use the string literal for this type's FromStr impl";
+                );
+            }
+        };
 
+        // try to parse type
         match <$ty>::from_str(inval.value().as_str()) {
+            // if good, turn it into the right token stream
             Ok(v) => $fn(Some(v)).into(),
             Err(e) => {
+                // otherwise emit the parse error with the parser's error message
+                // as well as make the compile error show the invalid string
                 emit_error!(inval, e);
+                // return dummy value of the right type to squash subsequent errors
                 $fn(None).into()
             }
         }
     }};
 }
 
+// IP types
+
 fn ip4_tokens(addr: Option<Ipv4Addr>) -> proc_macro2::TokenStream {
     let addr = addr.unwrap_or(Ipv4Addr::UNSPECIFIED);
-    let [a, b, c, d] = addr.octets();
+    let octets = addr.octets();
 
-    quote! { ::std::net::Ipv4Addr::new(#a, #b, #c, #d) }
+    quote! { ::std::net::Ipv4Addr::new(#(#octets),*) }
 }
 
 fn ip6_tokens(addr: Option<Ipv6Addr>) -> proc_macro2::TokenStream {
     let addr = addr.unwrap_or(Ipv6Addr::UNSPECIFIED);
-    let [a, b, c, d, e, f, g, h] = addr.segments();
+    let segments = addr.segments();
 
-    quote! { ::std::net::Ipv6Addr::new(#a, #b, #c, #d, #e, #f, #g, #h) }
+    quote! { ::std::net::Ipv6Addr::new(#(#segments),*) }
 }
 
 fn ipaddr_tokens(addr: Option<IpAddr>) -> proc_macro2::TokenStream {
@@ -78,10 +82,13 @@ pub fn ip6(input: TokenStream) -> TokenStream {
     parse_type!(input, ip6_tokens, Ipv6Addr)
 }
 
+// SocketAddr types
+
 fn sock4_tokens(addr: Option<SocketAddrV4>) -> proc_macro2::TokenStream {
     let addr = addr.unwrap_or(SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, 0));
     let ip = ip4_tokens(Some(*addr.ip()));
     let port = addr.port();
+
     quote! { ::std::net::SocketAddrV4::new(#ip, #port) }
 }
 
@@ -89,6 +96,7 @@ fn sock6_tokens(addr: Option<SocketAddrV6>) -> proc_macro2::TokenStream {
     let addr = addr.unwrap_or(SocketAddrV6::new(Ipv6Addr::UNSPECIFIED, 0, 0, 0));
     let ip = ip6_tokens(Some(*addr.ip()));
     let port = addr.port();
+
     quote! { ::std::net::SocketAddrV6::new(#ip, #port, 0, 0) }
 }
 
@@ -96,12 +104,12 @@ fn sockaddr_tokens(addr: Option<SocketAddr>) -> proc_macro2::TokenStream {
     let addr = addr.unwrap_or(SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, 0)));
     match addr {
         SocketAddr::V4(sock) => {
-            let sock_tok = sock4_tokens(Some(sock));
-            quote! { ::std::net::SocketAddr::V4(#sock_tok) }
+            let inner = sock4_tokens(Some(sock));
+            quote! { ::std::net::SocketAddr::V4(#inner) }
         }
         SocketAddr::V6(sock) => {
-            let sock_tok = sock6_tokens(Some(sock));
-            quote! { ::std::net::SocketAddr::V6(#sock_tok) }
+            let inner = sock6_tokens(Some(sock));
+            quote! { ::std::net::SocketAddr::V6(#inner) }
         }
     }
 }
@@ -124,6 +132,8 @@ pub fn sock6(input: TokenStream) -> TokenStream {
     parse_type!(input, sock6_tokens, SocketAddrV6)
 }
 
+// IpNetwork types
+
 cfg_if::cfg_if! {
     if #[cfg(feature = "ipnet")] {
         use ipnetwork::{IpNetwork, Ipv4Network, Ipv6Network};
@@ -132,6 +142,7 @@ cfg_if::cfg_if! {
             let net = net.unwrap_or(Ipv4Network::new(Ipv4Addr::UNSPECIFIED, 0).unwrap());
             let ip = ip4_tokens(Some(net.ip()));
             let prefix = net.prefix();
+
             quote! { ipnetwork::Ipv4Network::new(#ip, #prefix).unwrap() }
         }
 
@@ -139,6 +150,7 @@ cfg_if::cfg_if! {
             let net = net.unwrap_or(Ipv6Network::new(Ipv6Addr::UNSPECIFIED, 0).unwrap());
             let ip = ip6_tokens(Some(net.ip()));
             let prefix = net.prefix();
+
             quote! { ipnetwork::Ipv6Network::new(#ip, #prefix).unwrap() }
         }
 
@@ -148,12 +160,12 @@ cfg_if::cfg_if! {
             ));
             match net {
                 IpNetwork::V4(net) => {
-                    let net_tok = net4_tokens(Some(net));
-                    quote! { ipnetwork::IpNetwork::V4(#net_tok) }
+                    let inner = net4_tokens(Some(net));
+                    quote! { ipnetwork::IpNetwork::V4(#inner) }
                 }
                 IpNetwork::V6(net) => {
-                    let net_tok = net6_tokens(Some(net));
-                    quote! { ipnetwork::IpNetwork::V6(#net_tok) }
+                    let inner = net6_tokens(Some(net));
+                    quote! { ipnetwork::IpNetwork::V6(#inner) }
                 }
             }
         }
@@ -178,32 +190,36 @@ cfg_if::cfg_if! {
     }
 }
 
+// MacAddr types
+
 cfg_if::cfg_if! {
     if #[cfg(feature = "mac")] {
-        use macaddr::{MacAddr,MacAddr6,MacAddr8};
+        use macaddr::{MacAddr, MacAddr6, MacAddr8};
 
         fn mac6_tokens(addr: Option<MacAddr6>) -> proc_macro2::TokenStream {
             let addr = addr.unwrap_or(MacAddr6::from([0x00; 6]));
-            let [a, b, c, d, e, f] = addr.into_array();
-            quote! { macaddr::MacAddr6::new(#a, #b, #c, #d, #e, #f) }
+            let bytes = addr.into_array();
+
+            quote! { macaddr::MacAddr6::new(#(#bytes),*) }
         }
 
         fn mac8_tokens(addr: Option<MacAddr8>) -> proc_macro2::TokenStream {
             let addr = addr.unwrap_or(MacAddr8::from([0x00; 8]));
-            let [a, b, c, d, e, f, g, h] = addr.into_array();
-            quote! { macaddr::MacAddr8::new(#a, #b, #c, #d, #e, #f, #g, #h) }
+            let bytes = addr.into_array();
+
+            quote! { macaddr::MacAddr8::new(#(#bytes),*) }
         }
 
-        fn mac_tokens(net: Option<MacAddr>) -> proc_macro2::TokenStream {
-            let net = net.unwrap_or(MacAddr::V6(MacAddr6::from([0x00; 6])));
-            match net {
-                MacAddr::V6(net) => {
-                    let net_tok = mac6_tokens(Some(net));
-                    quote! { macaddr::MacAddr::V6(#net_tok) }
+        fn mac_tokens(addr: Option<MacAddr>) -> proc_macro2::TokenStream {
+            let addr = addr.unwrap_or(MacAddr::V6(MacAddr6::from([0x00; 6])));
+            match addr {
+                MacAddr::V6(addr) => {
+                    let inner = mac6_tokens(Some(addr));
+                    quote! { macaddr::MacAddr::V6(#inner) }
                 }
-                MacAddr::V8(net) => {
-                    let net_tok = mac8_tokens(Some(net));
-                    quote! { macaddr::MacAddr::V8(#net_tok) }
+                MacAddr::V8(addr) => {
+                    let inner = mac8_tokens(Some(addr));
+                    quote! { macaddr::MacAddr::V8(#inner) }
                 }
             }
         }
@@ -233,6 +249,8 @@ mod tests {
     #[test]
     fn compilation() {
         let t = trybuild::TestCases::new();
+        t.compile_fail("tests/fail/bad_tt.rs");
+
         t.compile_fail("tests/fail/ip.rs");
         t.compile_fail("tests/fail/ip4.rs");
         t.compile_fail("tests/fail/ip6.rs");
