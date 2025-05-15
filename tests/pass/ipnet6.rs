5b7e52278076bfdbabad1ac5491d4f8275ba1bea
Erich Heine <sophacles@gmail.com>
2025-05-14 20:19:42 -0500

    feat: add support for `ipnet` types
    Fixes: #6

---
+++ tests/pass/ipnet6.rs
@@ -0,0 +1,8 @@
+use ipnet::Ipv6Net;
+
+use const_addrs::{ip6, net6};
+
+fn main() {
+    let a = ipnet6!("2001:db8::32:23/63");
+    assert_eq!(a, Ipv6Net::new(ip6!("2001:db8::32:23"), 63).unwrap());
+}
